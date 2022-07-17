27d806c2a2d06732713fb354ad257b64e51b21af
satabios <max.satabiossathya@gmail.com>
2022-07-17 10:12:01 +0530

    updated UCSC status

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-239b1b42930a38436fc2.js"
+    "url": "webpack-runtime-d5e222a328c6e6d38017.js"
   },
   {
     "url": "framework-da70664296406da4ec21.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2272923d080d1847ec79f5ce2f98c733"
+    "revision": "d2417c586dfcaa41600aa92b69352076"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-36e198f4b34ea64c65e9.js"
